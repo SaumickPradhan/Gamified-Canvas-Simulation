7ec537890b509a22f44ea47e62d92332bc5cc45f
Saumick P Pradhan <85262444+SaumickPradhan@users.noreply.github.com>
2023-11-20 19:56:33 -0500

    For Vercel Update vite.config.js

--- Gamified-Canvas-Simulation/vite.config.js
+++ Gamified-Canvas-Simulation/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  base: "./",
+  optimizeDeps: {
+    disabled: false,
+  },
+  build: {
+    commonjsOptions: {
+      include: [],
+    },
+  },
+});
