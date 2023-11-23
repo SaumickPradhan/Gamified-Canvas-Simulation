a2178e1f7a1a9a58eac77819f362b2e170b44a36
Saumick P Pradhan <85262444+SaumickPradhan@users.noreply.github.com>
2023-11-22 22:04:37 -0500

    Update vite.config.js

--- Gamified-Canvas-Simulation/vite.config.js
+++ Gamified-Canvas-Simulation/vite.config.js
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./",
-  optimizeDeps: {
-    disabled: false,
-  },
-  build: {
-    commonjsOptions: {
-      include: [],
-    },
-  },
-});
+  plugins: [react()],
+})
+
+
+
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "tailwindcss";
+// import { defineConfig } from "vite";
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "./",
+//   optimizeDeps: {
+//     disabled: false,
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [],
+//     },
+//   },
+// });
